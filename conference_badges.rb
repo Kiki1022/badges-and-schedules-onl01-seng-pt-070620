893488b2f5f9ff1baf30aab01637458df91d45d8
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-12 17:04:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do|name|
+    badges << badge_maker(name)
+end
+  badges
+end  
+
+def assign_rooms(name, number)
+  return "Hello, #{name}! You'll be assigned to room #{number}!"
+end
+  
+def room_assigment(item, index)
+  number +=
+  rooms = []
+  array.each_with_index {|item, index| room[item] = index}
+    rooms << assign_rooms(name, number)
+  end
+  rooms
+
+end
+
+
+
+
+
